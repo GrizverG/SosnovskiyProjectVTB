b4117c6c59c73c4534e9d6f07458f2bf8622715f
grizverg <grishesaur@mail.ru>
2020-07-02 23:48:28 +0300

    Add article and article manager

---
+++ SosnovskiyProjectVTB/Controller/ArticleManager.swift
@@ -0,0 +1,89 @@
+//
+//  ArticleManager.swift
+//  SosnovskiyProjectVTB
+//
+//  Created by Gregory Pinetree on 02.07.2020.
+//  Copyright © 2020 Gregory Pinetree. All rights reserved.
+//
+
+import Foundation
+
+// #MARK: Get URL
+public func getURL(index: Int, rubric: Int) -> URL {
+    return URL(string: "https://news.myseldon.com/api/Section?rubricId=\(rubric)&pageSize=8&pageIndex=\(index)")!
+}
+
+public func getURL(task: String) -> URL{
+    return URL(string: "https://news.myseldon.com/news/search?text=\(task)")!
+}
+
+// MARK: - ArticleManager
+final public class ArticleManager {
+    
+    //
+    // MARK: - Constants
+    //
+    let defaultSession = URLSession(configuration: .default)
+    
+    //
+    // MARK: - Variables And Properties
+    //
+    var dataTask: URLSessionDataTask?
+    private var articleArray: [Article] = []
+    var ArticleArray: [Article] {
+        get {
+            return articleArray
+        }
+        
+        set(articles) {
+            articleArray = articles
+            // #TODO: Update tableView
+        }
+    }
+    
+    func updateNewsList(url: URL) {
+        
+        fetchNews(url: url) { [weak self] results in
+            if let results = results {
+                self?.ArticleArray = results
+            }
+        }
+    }
+    
+    //
+    // MARK: - fetchNews
+    //
+    private func fetchNews(url: URL, completion: @escaping ([Article]?) -> Void) {
+        dataTask?.cancel()
+        dataTask = defaultSession.dataTask(with: url) { [weak self] data, response, error in
+            defer {
+                self?.dataTask = nil
+            }
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            if let data = data {
+                self?.loadNews(data)
+            }
+        }
+        dataTask?.resume()
+    }
+    
+    //
+    // MARK: - loadNews
+    //
+    private func loadNews(_ data: Data) {
+        articleArray.removeAll()
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        do {
+            let articleList: ArticleList = try decoder.decode(ArticleList.self, from: data)
+            ArticleArray = articleList.items
+        } catch {
+            
+        }
+    }
+}
---
+++ SosnovskiyProjectVTB/Model/Article.swift
@@ -0,0 +1,112 @@
+//
+//  Article.swift
+//  SosnovskiyProjectVTB
+//
+//  Created by Gregory Pinetree on 30.06.2020.
+//  Copyright © 2020 Gregory Pinetree. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+fileprivate var requestID = ""
+
+// #MARK: ArticleList
+public struct ArticleList: Decodable {
+    var items: [Article]
+    var requestId: String
+    
+    enum CodingKeys: String, CodingKey {
+        case items = "news"
+        case requestId
+    }
+    
+    public init (from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.requestId = try container.decode(String.self, forKey: .requestId)
+        self.items = try container.decode([Article].self, forKey: .items)
+        
+        requestID = self.requestId
+    }
+}
+
+// #MARK: Article
+struct Article: Decodable {
+    var newsID: Int = 0
+    var timeToRead: String = ""
+    var sourceName: String = ""
+    var imageURL: ImageURL = ImageURL()
+    var header: String = ""
+    var publicationDate: String = ""
+    var image: UIImage = UIImage(named: "backupImage")!
+    
+    var seldonURL: String {
+        "https://news.myseldon.com/ru/news/index/\(newsID)?requestId=\(requestID)"
+    }
+
+    enum imageKey: String, CodingKey {
+        case url
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case newsId
+        case header = "title"
+        case timeToRead = "timeOfReading"
+        case sourceName
+        case publicationDate = "date"
+        case img
+    }
+    
+    // #MARK: Article init
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let newsID = try container.decode(Int.self, forKey: .newsId)
+        let header = try container.decode(String.self, forKey: .header)
+        let timeToRead = try container.decode(String.self, forKey: .timeToRead)
+        let publicationDate = try container.decode(String.self, forKey: .publicationDate)
+        let sourceName = try container.decode(String.self, forKey: .sourceName)
+        
+        self.newsID = newsID
+        self.header = prepareHeader(header)
+        self.timeToRead = timeToRead
+        self.publicationDate = preparePublicationDate(publicationDate)
+        self.imageURL = try container.decode(ImageURL.self, forKey: .img)
+        self.sourceName = sourceName
+        
+        if let image = loadImage() {
+            self.image = image
+        }
+    }
+    
+    private func prepareHeader(_ header: String) -> String {
+        let title = header.replacingOccurrences(of: "<b>", with: "")
+        return title.replacingOccurrences(of: "</b>", with: "")
+    }
+    
+    // #TODO: Change language of the date
+    private func preparePublicationDate(_ date: String) -> String {
+        // let dateFormatter = DateFormatter()
+        // dateFormatter.timeZone = .current
+        // dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss" //"2020-07-01T12:05:00"
+        // return "\(dateFormatter.date(from:date)!)"
+        return date
+    }
+    
+    private func loadImage() -> UIImage? {
+        if let data = try? Data(contentsOf: imageURL.url) {
+            if let receivedImage = UIImage(data: data) {
+                return receivedImage
+            }
+        }
+        return nil
+    }
+}
+
+// #MARK: ImageURL
+struct ImageURL: Codable {
+    var url: URL
+    
+    init() {
+        self.url = URL(fileURLWithPath: "")
+    }
+}
